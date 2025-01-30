7ea364ead984d663ca92cd4cb0b4351f6ebb7141
JasonSkog <jason.skog@gmail.com>
2025-01-30 13:08:44 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InitialCountsGame from './components/InitialCountsGame';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <InitialCountsGame />
+    </div>
+  );
+}
+
+export default App;
